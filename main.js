9f051070cb12b5201d135d97023da6e8e06c4afd
Oleksandr Prudskyi <oleksandr.prudskyi@gmail.com>
2025-12-14 16:50:19 +0100

    Refactor randomGender function for simplicity

--- main.js
+++ main.js
@@ -91,9 +91,6 @@ function generateEmployees(count, minAge, maxAge) {
     }
     return employees;
 }
-function randomGender(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
 function randomGender() {
     return Math.random() < 0.5 ? Gender.MALE : Gender.FEMALE;
 }
